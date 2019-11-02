e9db9b02b2ecb1c68faced3f05595cf248728f42
Qstick <qstick@gmail.com>
2019-11-02 15:32:05 -0400

    New: Scrape Cast/Crew/Collection Data on Movie Refresh

---
+++ src/NzbDrone.Core/Movies/Cast.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NzbDrone.Core.Movies
+{
+    public class Cast : Person
+    {
+        public string Character { get; set; }
+        public int Order { get; set; }
+    }
+}
---
+++ src/NzbDrone.Core/Movies/Crew.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NzbDrone.Core.Movies
+{
+    public class Crew : Person
+    {
+        public string Department { get; set; }
+        public string Job { get; set; }
+    }
+}
